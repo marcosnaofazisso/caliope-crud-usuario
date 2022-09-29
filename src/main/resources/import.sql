2bec44b720dd26f15f2cab2625eab814eb5c6f17
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-09-29 13:16:34 -0300

    add more tests users

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,13 @@
-insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Marcos Vinicius Ferreira', '34534534599', '11989332002', 'marcosvinicius@caliope.com.br', '1213456789');
-insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Marcos Maciel', '35635635699', '11989332002', 'marcosmaciel@caliope.com.br', '1213456789');
-insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Cristine Acoccela', '34234234299', '11989332002', 'cristine@caliope.com.br', '1213456789');
-insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Priscila Nastacio', '32232232299', '11989332002', 'priscila@caliope.com.br', '1213456789');
-insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Jonathan Felix', '36736736799', '11989332002', 'jonathan@caliope.com.br', '1213456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Marcos Vinicius Ferreira', '34534534599', '11989332002', 'marcosvinicius@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Marcos Maciel', '35635635699', '11989332002', 'marcosmaciel@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Cristine Acoccela', '34234234299', '11989332002', 'cristine@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Priscila Nastacio', '32232232299', '11989332002', 'priscila@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Jonathan Felix', '36736736799', '11989332002', 'jonathan@caliope.com.br', '123456789');
+
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Thiago Yamamoto', '36736736799', '11989332002', 'thiago@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Arnaldo Alves', '36736736799', '11989332002', 'arnaldo@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Henrique Ferreira', '36736736799', '11989332002', 'henrique@caliope.com.br', '123456789');
+
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Bruno Alves', '36736736799', '11989332002', 'bruno@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Déborah Oliveira', '36736736799', '11989332002', 'deborah@caliope.com.br', '123456789');
+insert INTO T_CALI_USUARIO (nm_usuario, cpf_usuario, cel_usuario, ds_email, ds_senha) VALUES ('Marcelo Braga', '36736736799', '11989332002', 'marcelo@caliope.com.br', '123456789');
